18d5cbb8d0b43176788c63b628326f5d393a4f65
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-03-04 21:04:45 -0500

    added helpers for dates and plural comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const helpers = require('./utils/helpers');
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
