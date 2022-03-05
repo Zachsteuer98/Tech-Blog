c7474a585345051d2b7a3af65164740fd2ac0450
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-03-04 22:20:54 -0500

    added auth for posts PUT POST and DELETE along with access to the Dashboard.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,8 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth')
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -64,7 +66,7 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.post('/', (req, res) => {
+  router.post('/', withAuth, (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_text: 'test test test', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -78,7 +80,7 @@ router.get('/', (req, res) => {
       });
   });
   
-  router.put('/:id', (req, res) => {
+  router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -102,7 +104,7 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
